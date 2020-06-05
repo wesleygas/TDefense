82e6ee23a65479e2bc226653916d7a89083edc8b
Fogelman <ddfogelman@gmail.com>
2020-06-04 23:15:06 -0300

    ADD dinamic map

---
+++ Assets/Scripts/RandomTiles.cs
@@ -0,0 +1,63 @@
+// tested with unity version: 2017.2.0b4
+// info: Fills tilemap with random tiles
+// usage: Attach this script to empty gameobject, assign some tiles, then press play
+// please make sure that you have at least version 2017.2 or the experimental 2d unity 5_5
+// https://forum.unity3d.com/threads/update-july-2017.484397/
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomTiles : MonoBehaviour {
+
+    public Tile[] tiles;
+    void Start () {
+
+        RandomTileMap ();
+
+    }
+
+    void RandomTileMap () {
+        // validation
+        if (tiles == null || tiles.Length < 1) {
+            Debug.LogError ("Tiles not assigned", gameObject);
+            return;
+        }
+
+        var parent = transform.parent;
+        if (parent == null) {
+            var go = new GameObject ("Grid");
+            go.AddComponent<Grid> ();
+            transform.SetParent (go.transform);
+        } else {
+            if (parent.GetComponent<Grid> () == null) {
+                parent.gameObject.AddComponent<Grid> ();
+            }
+        }
+
+        TilemapRenderer tr = GetComponent<TilemapRenderer> ();
+        if (tr == null) {
+            tr = gameObject.AddComponent<TilemapRenderer> ();
+        }
+
+        Tilemap map = GetComponent<Tilemap> ();
+        if (map == null) {
+            map = gameObject.AddComponent<Tilemap> ();
+        }
+
+        // random map generation
+        Vector3Int tilePos = Vector3Int.zero;
+        int index = 1;
+        int height = Maps.bound (1);
+        int width = Maps.bound (2);
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                tilePos.y = (height / 2) - i;
+                tilePos.x = j - (width / 2);
+
+                if (Maps.maps[index, i, j] == 0)
+                    map.SetTile (tilePos, tiles[0]);
+            }
+        }
+    }
+}
