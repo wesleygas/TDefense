8e968f214f2bfad910cac54d8960aeef42e66f31
wesleygas <wesleygl16@gmail.com>
2020-06-09 11:58:53 -0300

    spawning torres

---
+++ Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool hasTower = false;
+    public GameObject Tower;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTower(){
+        if(!hasTower){
+            hasTower=true;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            Instantiate(Tower,mousePos,Quaternion.identity);
+        }
+        /*else{
+            //Troca a torre na mão
+        }
+        */
+    }
+}
