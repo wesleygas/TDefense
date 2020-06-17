a432b76745c9b41aac82b90bef94ffc1e5c8d8c8
Fogelman <ddfogelman@gmail.com>
2020-06-17 15:16:17 -0300

    UPDATE add flying enemy

---
+++ Assets/Scripts/EnemyFly.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyFLy : MonoBehaviour
+{
+    private Animator animator;
+    private Transform target;
+
+    public int lifes = 100;
+    public int coins = 100;
+    public int damage = 1;
+    public int speed = 10;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        target = GameObject.Find("target").transform;
+    }
+
+    public void Damage(int damage = 1)
+    {
+        lifes -= damage;
+        if (lifes < 0)
+        {
+            animator.SetBool("dead", true);
+            StartCoroutine(Die());
+        }
+    }
+
+    public void Update()
+    {
+        if (target.position <= transform.position)
+        {
+            GameState.habitants -= damage;
+            Destroy(gameObject);
+        }
+
+        transform.position.y += speed * Time.deltaTime;
+    }
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1f);
+        GameState.coins += coins;
+        Destroy(gameObject);
+    }
+}
